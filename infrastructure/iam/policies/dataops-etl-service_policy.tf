1f7d30e945595d3e88fbfe5f49ca8134866aed2b
Colclough Doran <devops.codo@pm.me>
2026-05-17 20:44:11 +0100

    Imported dataops-ecs-task-executio IAM role

--- infrastructure/iam/policies/dataops-etl-service_policy.tf
+++ infrastructure/iam/policies/dataops-etl-service_policy.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_policy" "codebuild_dataops_etl" {
   name        = "codebuild-dataops-etl-service-role"
   description = "Policy for CodeBuild DataOps ETL service"
-  policy      = templatefile("policy-files/dataops-etl-service.json", {
+  policy = templatefile("policy-files/dataops-etl-service.json", {
     aws_account_id = data.aws_caller_identity.current.account_id
     aws_region     = var.aws_region
   })
