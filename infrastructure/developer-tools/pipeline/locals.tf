912cba238dcdf144fc0ad15a498843fdea663e68
Colclough Doran <devops.codo@pm.me>
2026-05-17 21:24:29 +0100

    Renamed build project and modules ADded pipeline mod and project scaffolding

--- infrastructure/developer-tools/pipeline/locals.tf
+++ infrastructure/developer-tools/pipeline/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  service       = "developer-tools/code-build"
+  service       = "developer-tools/pipeline"
   terraform_url = "https://github.com/Colclough-Doran/dataops-terraform/tree/main/infrastructure/${local.service}"
 }
