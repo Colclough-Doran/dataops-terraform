d5ec5f50da85dc1219f1ed6d33ca24054ccd0758
Colclough Doran <devops.codo@pm.me>
2026-05-17 11:26:39 +0100

    Updated outputs

--- infrastructure/iam/policies/outputs.tf
+++ infrastructure/iam/policies/outputs.tf
@@ -2,6 +2,14 @@ output "codebuild_dataops_etl_service_arn" {
   value = aws_iam_policy.codebuild_dataops_etl.arn
 }
 
+output "codebuild_dataops_etl_service_name" {
+  value = aws_iam_policy.codebuild_dataops_etl.name
+}
+
 output "eks_admin_access_arn" {
   value = aws_iam_policy.eks_admin_access.arn
+}
+
+output "eks_admin_access_name" {
+  value = aws_iam_policy.eks_admin_access.name
 }
