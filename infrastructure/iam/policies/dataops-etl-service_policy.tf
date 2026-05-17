c518200e5d52310ec064e5753c39cb0340efb0c3
Colclough Doran <devops.codo@pm.me>
2026-05-17 18:15:50 +0100

    added vats to ecs service role policy

--- infrastructure/iam/policies/dataops-etl-service_policy.tf
+++ infrastructure/iam/policies/dataops-etl-service_policy.tf
@@ -1,5 +1,8 @@
 resource "aws_iam_policy" "codebuild_dataops_etl" {
   name        = "codebuild-dataops-etl-service-role"
   description = "Policy for CodeBuild DataOps ETL service"
-  policy      = file("policy-files/dataops-etl-service.json")
+  policy      = templatefile("policy-files/dataops-etl-service.json", {
+    aws_account_id = data.aws_caller_identity.current.account_id
+    aws_region     = var.aws_region
+  })
 }
