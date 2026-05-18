8060ddb7d134f442e4d2fb3701f73868dd4b0a22
Colclough Doran <devops.codo@pm.me>
2026-05-18 10:23:31 +0100

    Added GitHub-CICD IAM user Added github-cicd IAM policy Added Internal-Tools IAM group Updated user modules Updated Roles module

---
+++ infrastructure/iam/users/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  service       = "iam/users"
+  terraform_url = "https://github.com/Colclough-Doran/dataops-terraform/tree/main/infrastructure/${local.service}"
+}
--- modules/iam/users/main.tf
+++ modules/iam/users/main.tf
@@ -4,10 +4,17 @@ resource "aws_iam_user" "this" {
 
 resource "aws_iam_user_group_membership" "this" {
   user   = aws_iam_user.this.name
-  groups = var.group_name
+  groups = var.groups
 }
 
-resource "aws_iam_user_login_profile" "this" {
+resource "aws_iam_user_policy_attachment" "this" {
+  for_each = toset(var.iam_policy)
+
+  policy_arn = each.key
+  user       = aws_iam_user.this.name
+}
+
+/* resource "aws_iam_user_login_profile" "this" {
   user                    = aws_iam_user.this.name
   password_reset_required = true
 }
@@ -21,4 +28,4 @@ resource "aws_secretsmanager_secret" "this" {
   description = "Access Key ID for ${var.user_name}"
   kms_key_id  = var.kms_key_id
   #aws_iam_access_key.this.secret
-}
+} */
